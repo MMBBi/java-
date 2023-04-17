aa68106893247ccbf77f48e5f56c336cd3e11d0e
MMBBi <996552980@qq.com>
2023-04-17 22:38:23 +0800

    4月17日代码， java数据类型的取值范围验证

--- Test_4_16/HelloWorld.java
+++ Test_4_16/HelloWorld.java
@@ -1,7 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
-        System.out.println(1000);
-        }
+        System.out.println("HelloWorld" );
     }
 }
