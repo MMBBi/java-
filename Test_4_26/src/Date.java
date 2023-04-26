be9bb39b39cf889aa13938c1672109150ce1bf2b
MMBBi <996552980@qq.com>
2023-04-26 20:09:03 +0800

    4月26日，this引用的三个用法 和对象的构造与初始化

---
+++ Test_4_26/src/Date.java
@@ -0,0 +1,55 @@
+public class Date {
+
+    public int year;
+
+    public int month;
+
+    public int day;
+
+    /*public void setDay(int y, int m, int d) {
+        year = y;
+        month = m;
+        day = d;
+
+    }*/
+
+
+    //this引用的使用发放
+    //方法1：给当前对象的属性赋值
+    public void setDay(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
+
+    public void printDay() {
+        System.out.println(year+"/"+month+"/"+day);
+    }
+
+
+    public Date(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        System.out.println("构造方法被调用了");
+        System.out.println("================================");
+
+        //this访问成员方法
+        this.printDay();
+        System.out.println("通过this访问了成员方法");
+        System.out.println("================================");
+    }
+
+    public Date() {
+        //方法2：访问其他构造方法
+        //必须是构造方法中的第一条语句
+        //不能形成环
+        this(2023,4,26);
+        System.out.println("通过this访问了有参数的构造方法");
+        System.out.println("================================");
+
+
+    }
+
+
+}
---
+++ Test_4_26/src/Main.java
@@ -0,0 +1,45 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Date date1 = new Date();
+    }
+
+    public static void main4(String[] args) {
+        Date date1 = new Date();
+        date1.printDay();
+    }
+
+    public static void main3(String[] args) {
+        Date date1 = new Date(2023,4,26);
+        date1.printDay();
+
+    }
+
+    public static void main2(String[] args) {
+        Date date1 = new Date();
+        date1.printDay();
+        date1.setDay(2023,4,26);
+        date1.printDay();
+    }
+
+    public static void main1(String[] args) {
+        Date date1 = new Date();
+        Date date2 = new Date();
+        Date date3 = new Date();
+
+        date1.setDay(2023, 4, 26);
+        date2.setDay(2023, 4, 27);
+        date3.setDay(2023, 4, 28);
+
+        date1.printDay();
+        date2.printDay();
+        date3.printDay();
+
+    }
+
+
+
+
+
+
+}
