fbd07f9ac8b3a3538097f791455b4a1f875b0e85
MMBBi <996552980@qq.com>
2023-04-27 22:21:19 +0800

    4月27日，访问限定符和包

---
+++ Test_4_27/src/Test1/Test2/Main2.java
@@ -0,0 +1,36 @@
+package Test1.Test2;
+
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextInt()) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            Date date = new Date(x, y);
+
+            System.out.println(date.getX()+date.getY());
+        }
+    }
+
+}
+
+class Date {
+    int x;
+    int y;
+
+    public Date(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+
+    public int getY() {
+        return y;
+    }
+}
